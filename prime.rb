fc2d22d161944fb1fbdedf7efa30e8341971c76b
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-15 20:23:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 def prime?(num)
-  (2..(num)).each do |n|
-    return false if num % n == 0
-  end
-  true
+  (2..(num - 1)).each do |n|
+    if num % n == 0
+      return false 
+  else if num % n > 0
+    return true
+end
 end
