599334ce678b14603f3f0a0ac651ea0f87892a68
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-15 20:51:39 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 def prime?(num)
   prime_array = []
   (2..(num - 1)).each{|n| prime_array << n}
-  if prime_array.any?{|p| num % p == }
-    if float?(final)  
+  if prime_array.any?{|p| num % p == 0}
       return false
     else
       return true
+    end
+  end
